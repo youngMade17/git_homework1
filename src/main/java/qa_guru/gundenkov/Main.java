5f89377da486c00f7c4a726bf6030c7f3466e59e
Михаил Гунденков <gundenkov.99@gmai.com>
2023-04-23 00:51:23 +0300

    commit 2.1

--- src/main/java/qa_guru/gundenkov/Main.java
+++ src/main/java/qa_guru/gundenkov/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!!!_Master change");
+        System.out.printf("Hello and welcome!!!___2nd change");
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 2; i <= 5; i++) {
